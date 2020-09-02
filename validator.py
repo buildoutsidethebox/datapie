b87233042165fe96becd0ccf18f0a3aea3919865
Rudolf Virchow <rudolfvirchow@Vons-MacBook-Pro.local>
2020-09-02 12:07:52 +0800

    Uploaded first ods

---
+++ validator.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from csv import DictReader
+
+
+class Validator:
+    "Manual validator of IMDB"
+
+    def __init__(self):
+        self._initialize()
+    
+    def _initialize(self):
+        self.bot = webdriver.Chrome()
+        self.bot.fullscreen_window()
+
+    def access(self, imdb):
+        self.bot.get(f'https://www.imdb.com/title/{imdb[1]}/')
+        try:
+            cont = input(f"Match {imdb[0]}?\n")
+        except:
+            self.bot.quit()
+
+        if cont == "":
+            print("Moving to another IMDB!")
+
+    def get_imdb(self,csv):
+        # imdbs
+        imdbs = []
+        with open(csv, "r") as file:
+            table = DictReader(file, delimiter="|")
+            for row in table:
+                # print(row)
+                i = [None, None]
+                i[0] = row["title"]
+                i[1] = row["imdb_number"]
+                imdbs.append(i)
+
+            return imdbs
+
+
+if __name__ == "__main__":
+    imdb = Validator()
+
+    imdbs = imdb.get_imdb("peacocktv_front_2020.08.21_imdb.csv")
+    i = 332
+    while i < len(imdbs):
+        imdb.access(imdbs[i])
+        i += 1
+
+
+           
+
